a56b16ab1d37b4eeecc70a3a5acfef5b708da50a
Tucker Legard <tlegard@vt.edu>
2014-05-05 17:19:34 -0400

    documetnation and cleanup of python script

--- watchForNew.py
+++ watchForNew.py
@@ -1,3 +1,6 @@
+# -----------------------------------------------------------------------------#
+#   Required Packages                                                          #
+# -----------------------------------------------------------------------------#
 import sys
 import time
 import logging
@@ -10,6 +13,16 @@ from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 from pymongo import MongoClient
 
+# -----------------------------------------------------------------------------#
+#   CONFIG                   Please change me                                  #
+# -----------------------------------------------------------------------------#
+PREMEIRE_PATH = ""                  # the path to adobe media encoder's log file
+XML_DESTINATION_PATH = ""           # the directory where you would like to move
+                                    # the xml files too
+
+# -----------------------------------------------------------------------------#
+#   Connect to Database                                                        #
+# -----------------------------------------------------------------------------#
 client = None
 try:
     client = MongoClient('localhost', 27017)
@@ -18,41 +31,51 @@ except:
     sys.exit(0);
 
 db = client.library
+
+# -----------------------------------------------------------------------------#
+#   Load Stop Words                                                            #
+# -----------------------------------------------------------------------------#
 stopWords = stopwords.words('english')
+
 
 class MyHandler(FileSystemEventHandler):
     patterns = ["AMEEncodingLog.txt"]
 
+    # -------------------------------------------------------------------------#
+    # Import into database on log update                                       #
+    # -------------------------------------------------------------------------
     def process(self, event):
-        """
-        event.event_type 
-            'modified' | 'created' | 'moved' | 'deleted'
-        event.is_directory
-            True | False
-        event.src_path
-            path/to/observed/file
-        """
-        # the file will be processed there
+        # Read in the contents from the log file
         logfile = io.open(event.src_path, 'r+', encoding="utf-16")
         data = logfile.read()
         if not data:
             return
 
+        # Look for the specific entry in the log file that means a subtitle
+        # _stt.xml file was created. 
         p = re.compile("Source File: (.+)\n - Output File: (.+)")
         couplets = p.findall(data)
-        
+    
 
+        # For each of the log files created
         for couplet in couplets: 
+            # Find the .cit file and change it's extension to _sst.xml
+            # move this file to XML_DESTINATION_PATH
             if re.match("\S+\.cit$", couplet[1]):
                 toName = os.path.basename(couplet[0])
-                destination = 'D:\\library\\xml files\\' + toName.split('.')[0] + '.xml'
+                destination = XML_DESTINATION_PATH + toName.split('.')[0] + '.xml'
                 xmlFile = couplet[1].split('.')[0] + '_stt.xml';
-                print xmlFile
+                print xmlFile + "was moved"
                 try:
                     os.rename(xmlFile, destination);
                 except:
                     sys.exit(0);
 
+            # --------------------------------------------------------------------------------
+            # Convert the XML into JSON
+            #   ~ Essentially boing down the <Name>Confidence</Name><Value>100</Value> into
+            #     more lightweight JSON {confidence: value}
+            # --------------------------------------------------------------------------------
             xmlfile = io.open(destination, 'r');
             xmlData = xmlfile.read();
 
@@ -65,8 +88,8 @@ class MyHandler(FileSystemEventHandler):
                 confidence = None
                 duration = None
 
+                # skip stop words
                 if name in stopWords:
-                    print "skipping" + name
                     continue
 
                 time = cue.find('time').string
@@ -88,18 +111,21 @@ class MyHandler(FileSystemEventHandler):
                     continue
 
 
+            # JSON file to insert
             json = {
                 "name": toName.split('.')[0],
                 "list": cuelist
             }
 
+            # Put it in the database!
             try:
                 db.videos.insert(json);
                 print "inserted"
             except: 
                 print "Couldn't insert"
                 sys.exit(0)
-            
+        
+        # Clear the log file so we don't reimport the XML file that happened
         logfile.seek(0)
         logfile.truncate()
         logfile.close()
@@ -107,14 +133,16 @@ class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
         self.process(event)
 
+# -----------------------------------------------------------------------------#
+#   Watch for file changes and response according the method described above   #
+# -----------------------------------------------------------------------------#
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    path = 'C:\\Users\\Tucker\\AppData\\Roaming\\Adobe\\Adobe Media Encoder\\6.0'
     event_handler = MyHandler();
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, REMEIRE_PATH, recursive=True)
     observer.start()
     try:
         while True:
